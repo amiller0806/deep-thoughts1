d776247aae904c5ffce351bce9a97a46585bf6b7
amiller0806 <ariel.miller555@gmail.com>
2023-02-06 13:29:15 -0500

    tested queries for user and thoughts

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,47 @@
+const { User, Thought } = require('../models');
+// resolver can accept four arguments in the following order:
+
+// parent: This is if we used nested resolvers to handle more complicated actions, as it would hold the reference to the resolver that executed the nested resolver function. We won't need this throughout the project, but we need to include it as the first argument.
+
+// args: This is an object of all of the values passed into a query or mutation request as parameters. In our case, we destructure the username parameter out to be used.
+
+// context: This will come into play later. If we were to need the same data to be accessible by all resolvers, such as a logged-in user's status or API access token, this data will come through this context parameter as an object.
+
+// info: This will contain extra information about an operation's current state. This isn't used as frequently, but it can be implemented for more advanced uses.
+
 const resolvers = {
+  // Here, we pass in the parent as more of a placeholder parameter.
+  // It won't be used, but we need something in that first parameter's spot
+  // so we can access the username argument from the second parameter.
+  //  We use a ternary operator to check if username exists.
+  // If it does, we set params to an object with a username key set to that
+  // value. If it doesn't, we simply return an empty object.
+  // We then pass that object, with or without any data in it, to our .find() method
+  // If there's data, it'll perform a lookup by a specific username.
   Query: {
-    helloWorld: () => {
-      return "Hello world!";
+    // get all users
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('thoughts')
+        .populate('friends');
     },
-  },
+
+    // get user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    }
+  }
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,55 @@
-// import the gql tagged template function 
+// import the gql tagged template function
 
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs 
+// create our typeDefs
+// type Query
+// allow us to query thoughts with or without the username parameter
+
 const typeDefs = gql`
+  ##  if we want to look up a single thought or user,
+  ## we need to know which one we're looking up and thus
+  ## necessitate a parameter for us to look up that data.
+
+  ## define that a user will return all the data in their Mongoose model.
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    #  notice the thoughts field is an array of Thought types
+    thoughts: [Thought]
+    # Notice: friends field is an array that will be populated with data that
+    # also adheres to the User type, as a user's friends should follow the
+    # same data pattern as that user.
+    friends: [User]
+  }
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
   type Query {
-    helloWorld: String
+    users: [User]
+    ## Notice the exclamation point ! after the query parameter data type definitions? That indicates that
+    ## for that query to be carried out, that data must exist. Otherwise, Apollo will return an error to
+    ## the client making the request and the query won't even reach the resolver function associated with it.
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
   }
 `;
 
